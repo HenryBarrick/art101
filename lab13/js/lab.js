52a01a7f54406eb2d164ba8f513ccbc38285de39
Henry Barrick <tripp.barrick@gmail.com>
2023-11-22 18:27:17 -0800

    Lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,4 +1,24 @@
-// index.js - explanation
+// index.js - This is code for lab 13 which is focused on using loops.
 // Author: Henry Tripp Barrick and Tomoya Kato
 // Date: November  2023
+
+$("#button").click(function(){
+    for (var itr = 1; itr <= 200; itr++){
+        var moth = "";
+        
+        if ((itr % 3) == 0){
+        moth += "Fizz";
+        }
+        if ((itr % 5) == 0){
+        moth += "Buzz";
+        }
+        if ((itr % 7 ) == 0){
+        moth += "Boom";
+        }
+        if (moth.length <= 0){
+        moth = itr;
+        }
+
+        $("#output").append("<p>" + moth + "</pl>");
+}});
 
